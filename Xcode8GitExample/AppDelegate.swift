98ef321c88af24c3525be92e1f05ca92424ee672
Hrabsky, Tanya (NRK-OCT) <tanya.hrabsky@octagon.com>
2017-03-29 10:19:28 -0400

    Initial Commit

---
+++ Xcode8GitExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Xcode8GitExample
+//
+//  Created by Hrabsky, Tanya (NRK-OCT) on 3/29/17.
+//  Copyright © 2017 Hrabsky, Tanya (NRK-OCT). All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Xcode8GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8GitExample
+//
+//  Created by Hrabsky, Tanya (NRK-OCT) on 3/29/17.
+//  Copyright © 2017 Hrabsky, Tanya (NRK-OCT). All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
